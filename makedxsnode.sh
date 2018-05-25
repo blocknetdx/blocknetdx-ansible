1e309d22ff49c749990e34ba2af206e2e3646a1d
atcsecure <dmetcalf@atcsecure.com>
2018-05-25 07:36:51 -0400

    updated bash/ansible deploy scripts

--- makedxsnode.sh
+++ makedxsnode.sh
@@ -1,8 +1,53 @@
 #!/usr/bin/env bash
+# This script is still beta, please edit WALLETPATH to match your ENV
+#
+
+WALLETNAME=blocknetdx-cli
+WALLETPATH=/home/atcsecure/dx/blocknetdx-3.9.22/bin/
 mynodename=$1
-mynewnodeaccount=$(./blocknetdx-cli getaccountaddress ${mynodename})
+
+# create account using nodename
+mynewnodeaccount=$(${WALLETPATH}${WALLETNAME} getaccountaddress ${mynodename})
 echo "new account address ${mynewnodeaccount}"
-mynewkey=$(./blocknetdx-cli servicenode genkey)
+# send 5k to new address
+echo "sending"
+myvinhash=$(${WALLETPATH}${WALLETNAME} sendtoaddress ${mynewnodeaccount} 5000)
+# gen snode key
+echo "generating snode key"
+mynewkey=$(${WALLETPATH}${WALLETNAME} servicenode genkey)
+# get vout value
+echo "waiting..."
+sleep 90
+
 echo "new key ${mynewkey}"
-ansible-playbook -i ./hosts makedxsnode.yml -e "servicenodeprivkey=${mynewkey}"
+
+# Build snode via aws using ansible
+ansible-playbook -i ./hosts ./makedxsnode.yml -e "servicenodeprivkey=${mynewkey} snodename=${mynodename}"
+#load up ip 
+mynewip=$(head -n 1 ~/ansible/currentip.txt)
+echo "currentip: ${mynewip}"
+echo "waiting..."
+sleep 90
+
+echo "-----------" >> buildlog.txt
+echo $mynodename >> buildlog.txt
+echo $mynewip >> buildlog.txt
+echo $mynewnodeaccount >> buildlog.txt
+echo $myvinhash >> buildlog.txt
 echo $mynewkey >> buildlog.txt
+echo $myhashvoutvalue >> buildlog.txt
+
+echo "getting vout value"
+myhashvoutvalue=$(${WALLETPATH}${WALLETNAME} listunspent | grep ${mynewnodeaccount} -A 5 -B 2 | grep 'vout' | cut -d':' -f2-)
+# clean it up - remove first/last chars from string
+myhashvoutvalue=${myhashvoutvalue:1:${#myhashvoutvalue}-2}
+echo "vout ${myhashvoutvalue}"
+
+# lock it
+mylockresults=$(${WALLETPATH}${WALLETNAME} lockunspent false "[{\"txid\":\"${myvinhash}\",\"vout\":${myhashvoutvalue}}]")
+
+echo $mylockresults >> buildlog.txt
+echo "-----------" >> buildlog.txt
+echo "${mynodename} ${mynewip}:41412 $mynewkey $myvinhash $myhashvoutvalue">> snode.conf
+#echo $ansibleresults >> abuildresults.txt
+
